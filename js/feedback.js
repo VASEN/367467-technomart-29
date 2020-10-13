896c8327d4fc65b5e3142b1dadf87be235ee2c5d
ВАЛЕРИЙ <zaichkovv@mail.ru>
2020-10-14 00:03:47 +0300

    Доработан JS для модального окна обратной связи

--- js/feedback.js
+++ js/feedback.js
@@ -1,23 +1,66 @@
-let feedbackOpen = document.querySelector(".about-us__button_contacts");
-let feedbackForm = document.querySelector(".modal-feedback");
-let feedbackClose = feedbackForm.querySelector(".modal__button--close");
+const feedbackOpen = document.querySelector(".about-us__button_contacts");
+const feedbackModal = document.querySelector(".modal-feedback");
+const feedbackClose = feedbackModal.querySelector(".modal__button--close");
+const feedbackContent = feedbackModal.querySelector(".modal__content");
+const overlay = feedbackModal.querySelector(".modal__overlay");
 
-let feedbackName = feedbackForm.querySelector("[name=feedback__name]");
+const feedbackForm = feedbackModal.querySelector("form");
+const feedbackName = feedbackModal.querySelector("[name=feedback__name]");
+const feedbackEmail = feedbackModal.querySelector("[name=feedback__email]");
+const feedbackText = feedbackModal.querySelector("[name=feedback__text]");
 
+let storage = true;
+let storageName = "";
 
-feedbackOpen.addEventListener("click", function () {
-  feedbackForm.classList.add("modal__show");
-  feedbackName.focus();
-});
+try {
+  if (localStorage.getItem("name")) {
+    storageName = localStorage.getItem("name");
+  }
+} catch (err) {
+  storage = false;
+}
 
-feedbackForm.addEventListener("keydown", function (evt){
+feedbackOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
-  if (evt.code === "Escape" || evt.code === 27) {
-    feedbackForm.classList.remove("modal__show");
+  feedbackModal.classList.add("modal__show");
+  if (storageName) {
+    console.log(storageName);
+    feedbackName.value = storageName;
+    feedbackEmail.focus();
+  } else {
+    feedbackName.focus();
   }
 });
 
+feedbackForm.addEventListener("submit", function (evt) {
+  evt. preventDefault();
+  if (!feedbackName.value || !feedbackEmail.value || !feedbackText.value) {
+    feedbackContent.classList.remove("modal__shake");
+    feedbackContent.offsetWidth = feedbackContent.offsetWidth;
+    feedbackContent.classList.add("modal__shake");
+  } else {
+    if (storage) {
+      localStorage.setItem("name", feedbackName.value)
+      feedbackModal.classList.remove("modal__show");
+    }
+  }
+});
 
 feedbackClose.addEventListener("click", function () {
-  feedbackForm.classList.remove("modal__show");
+  feedbackModal.classList.remove("modal__show");
+  feedbackContent.classList.remove("modal__shake");
 });
+
+window.addEventListener("keydown", function (evt){
+  if (evt.code === "Escape" || evt.key == 27) {
+    if (feedbackModal.classList.contains("modal__show")) {
+      evt.preventDefault();
+      feedbackModal.classList.remove("modal__show");
+      feedbackContent.classList.remove("modal__shake");
+    }
+  }
+});
+
+overlay.addEventListener("click", function () {
+  feedbackModal.classList.remove("modal__show");
+})
