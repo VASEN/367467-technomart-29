882991657764b469e164c8de744dbc105c3a3d4d
ВАЛЕРИЙ <zaichkovv@mail.ru>
2020-10-14 00:16:49 +0300

    Доработан JS для модального окна корзины

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,30 @@
-let buyButton = document.querySelectorAll(".goods__button--buy");
-let cartForm = document.querySelector(".modal-cart");
-let cartClose = cartForm.querySelector(".modal__button--close");
+const cartOpen = document.querySelectorAll(".goods__button--buy");
+const cartModal = document.querySelector(".modal-cart");
+const cartClose = cartModal.querySelector(".modal__button--close");
+const cartOverlay = cartModal.querySelector(".modal__overlay");
 
-buyButton.forEach(function (item) {
-  item.addEventListener("click", function () {
-    cartForm.classList.add("modal__show");
+
+cartOpen.forEach(function (item) {
+  item.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartModal.classList.add("modal__show");
   })
 });
 
-cartClose.addEventListener("click", function () {
-  cartForm.classList.remove("modal__show");
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartModal.classList.remove("modal__show");
 });
+
+window.addEventListener("keydown", function (evt){
+  if (evt.code === "Escape" || evt.key == 27) {
+    if (cartModal.classList.contains("modal__show")) {
+      evt.preventDefault();
+      cartModal.classList.remove("modal__show");
+    }
+  }
+});
+
+cartOverlay.addEventListener("click", function () {
+  cartModal.classList.remove("modal__show");
+})
