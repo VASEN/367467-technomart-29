f8581a2754369b354802de4545cb77450f5d1081
ВАЛЕРИЙ <zaichkovv@mail.ru>
2020-10-17 16:29:11 +0300

    Двигается, но неправильно

--- js/range.js
+++ js/range.js
@@ -10,47 +10,117 @@ let priceMinInput = priceFieldset.querySelector("#min-price");
 let priceClientCoords = priceRange.getBoundingClientRect(); // координаты полоски диапазона
 let priceCoords = {};
 
-console.log(priceClientCoords);
+// console.log(priceClientCoords);
 // console.log(scrollY);
 // console.log(scrollX);
 
-priceCoords.top = priceClientCoords.top + scrollY;
-console.log(priceCoords.top);
-priceCoords.left = priceClientCoords.left + scrollX;
-console.log(priceCoords.left);
+// priceCoords.y = priceClientCoords.top + scrollY; // координата полоски Х с учетом прокрутки
+// // console.log(priceCoords.top);
+// priceCoords.x = priceClientCoords.left + scrollX; // координата полоски Y с учетом прокрутки
+// // console.log(priceCoords.left);
+
+let startCoords = {};
+startCoords.x = priceClientCoords.left + scrollX;
+startCoords.y = priceClientCoords.top + scrollY;
+
+let endCoords = {};
+endCoords.x = startCoords.x + priceClientCoords.width;
+endCoords.y = startCoords.y;
+
+console.log(startCoords);
+console.log(endCoords);
+
+let isMouseDown = false;
+
+let priceMinPointerCoords = "";
+let priceMinPointerStartCoords = {};
+
+let minShift = "";
 
 priceMinPointer.addEventListener("mousedown", function (evt) {
-  priceMinPointer.focus();
-  priceMinPointer.addEventListener("dragstart", function () {}, false);
-  let priceMinPointerClientCoords = priceMinPointer.getBoundingClientRect();
-  let priceMinPointerCoords = {};
+  priceMinPointer.focus()
+  isMouseDown = true;
 
-  priceMinPointerCoords.top = priceMinPointerClientCoords.top + scrollY;
-  console.log(priceMinPointerCoords.top);
-  priceMinPointerCoords.left = priceMinPointerClientCoords.left + scrollX;
-  console.log(priceMinPointerCoords.left);
+  priceMinPointerCoords = priceMinPointer.getBoundingClientRect();
+  console.log("EVT.X => " + evt.x);
+  minShift = priceMinPointerCoords.width / 2;
+  console.log("SHIFT => " + minShift);
 
-  debugger;
-  let right = priceRange.offsetWidth - priceMinPointer.offsetWidth;
-  console.log(right);
 
-  // debugger;
-  let shiftX = evt.pageX - priceMinPointerCoords.left;
 
-  document.addEventListener("mousemove", function (evt) {
-    let newLeft = evt.pageX - priceMinPointerCoords.left - shiftX;
-    if (newLeft < 0) newLeft = 0;
-    if (newLeft > right) newLeft = right;
-    priceMinPointer.style.left = newLeft + "px";
-    let result = Math.round(newLeft / right * 100);
-    priceMinInput.innerHTML = result.toString();
-  });
-}, false);
+  // let priceMinPointerStartCoords = {};
 
-document.addEventListener("mouseup", function () {
-  document.removeEventListener("mousemove", function () {});
-  document.removeEventListener("mouseup", function () {});
-  // document.onmousemove = document.onmouseup = null;s
+  // priceMinPointerStartCoords.y = priceMinPointerCoords.top + scrollY; // координата ползунка Х с учетом прокрутки
+  // console.log(priceMinPointerCoords.top);
+  // priceMinPointerStartCoords.x = priceMinPointerCoords.left + scrollX; // координата ползунка Y с учетом прокрутки
+  // console.log(priceMinPointerCoords.left);
 });
+
+priceRange.addEventListener("mousemove", function (evt) {
+  if (isMouseDown === true) {
+    // debugger;
+    let newX = "";
+    newX = evt.x;
+    console.log(priceClientCoords.x + "=>" + newX);
+    // if (newX < 0) {
+    //   newX = startCoords.x;
+    // }
+    // if (newX > priceClientCoords.width) {
+    //   newX = endCoords.x;
+    // }
+    let valueMin = newX - priceClientCoords.x;
+    priceMinPointer.style.left = (newX - priceClientCoords.x) + "px";
+    priceMinInput.value = valueMin;
+    console.log(priceMinPointer.style.left);
+  }
+});
+
+priceMinPointer.addEventListener("mouseup", function (evt){
+  // debugger;
+  isMouseDown = false;
+  priceRange.removeEventListener("mousemove", function (evt) {
+
+  });
+})
+
+
+
+function newPosition (startX, startY, endX) {
+
+}
+
+// priceMinPointer.addEventListener("mousedown", function (evt) {
+//   priceMinPointer.focus();
+//   priceMinPointer.addEventListener("dragstart", function () {}, false);
+//   let priceMinPointerClientCoords = priceMinPointer.getBoundingClientRect();
+//   let priceMinPointerCoords = {};
+//
+//   priceMinPointerCoords.top = priceMinPointerClientCoords.top + scrollY;
+//   console.log(priceMinPointerCoords.top);
+//   priceMinPointerCoords.left = priceMinPointerClientCoords.left + scrollX;
+//   console.log(priceMinPointerCoords.left);
+//
+//   debugger;
+//   let right = priceRange.offsetWidth - priceMinPointer.offsetWidth;
+//   console.log(right);
+//
+//   // debugger;
+//   let shiftX = evt.pageX - priceMinPointerCoords.left;
+//
+//   document.addEventListener("mousemove", function (evt) {
+//     let newLeft = evt.pageX - priceMinPointerCoords.left - shiftX;
+//     if (newLeft < 0) newLeft = 0;
+//     if (newLeft > right) newLeft = right;
+//     priceMinPointer.style.left = newLeft + "px";
+//     let result = Math.round(newLeft / right * 100);
+//     priceMinInput.innerHTML = result.toString();
+//   });
+// }, false);
+//
+// document.addEventListener("mouseup", function () {
+//   document.removeEventListener("mousemove", function () {});
+//   document.removeEventListener("mouseup", function () {});
+//   // document.onmousemove = document.onmouseup = null;
+// });
 
 
